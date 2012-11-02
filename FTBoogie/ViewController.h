a2fdea6638138b4f6943433ca2e41a3aa21ae9c1
Codemis <johnathan@missionaldigerati.com>
2012-11-01 19:46:02 -0700

    Initial Code.

--- FTBoogie/ViewController.h
+++ FTBoogie/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  FTBoogie
-//
-//  Created by Johnathan Pulos on 11/1/12.
-//  Copyright (c) 2012 Johnathan Pulos. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- FTBoogie/ViewController.m
+++ FTBoogie/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  FTBoogie
-//
-//  Created by Johnathan Pulos on 11/1/12.
-//  Copyright (c) 2012 Johnathan Pulos. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -13,17 +5,4 @@
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
